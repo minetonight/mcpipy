f52e5415562f7757136b642089242e283bf5a7c0
minetonight <aleksandarnp@gmail.com>
2014-04-17 12:45:07 +0300

    added possibility for prisms; Better comments;

--- aleks_pyramideBuilder.py
+++ aleks_pyramideBuilder.py
@@ -4,20 +4,27 @@ from math import *
 import random
 import server
 
+"""user variables"""
+# base center coordinates
 x0 = 321
 y0 = 119
 z0 = 86
 
-sideX = 150
-sideZ = 150
-slopeXRatio = 1
-slopeZRatio = 1
-material_id = 1 # TODO sandstone
+sideX = 20 # base dimentions
+sideZ = 25
+slopeXRatio = 1 # the vertical size of pyramide steps
+slopeZRatio = 2
+offsetX = 1 # the horisontal size of the pyramide steps
+offsetZ = 2
+#material_id = block.AIR.id # to clean what you did :D
+material_id = block.SANDSTONE.id
 
+# your world spown location
 spawnX = 200
 spawnY = 65
 spawnZ = 200
 
+"""eof user variables"""
 
 #calc starting corner point
 x1 = x0 - sideX/2 - spawnX
@@ -34,7 +41,7 @@ level = 0
 dx = 0
 dz = 0
 
-while widthX >= sideX/2 and widthZ >= sideZ/2:
+while (widthX >= sideX/2 or slopeXRatio < 1) and (widthZ >= sideZ/2 or slopeZRatio < 1):
   print "widthX=%s, widthZ=%s" % (widthX, widthZ)
   #put layer of the material
   mc.setBlocks(x1+dx, y1+level, z1+dz, x1+widthX, y1+level, z1+widthZ, material_id)
@@ -42,12 +49,12 @@ while widthX >= sideX/2 and widthZ >= sideZ/2:
   
   level += 1
   
-  if level % slopeXRatio == 0:
-    dx += 1
-    widthX -= 1
-  if level % slopeZRatio == 0:
-    dz += 1
-    widthZ -= 1
+  if slopeXRatio > 0 and level % slopeXRatio == 0:
+    dx += offsetX
+    widthX -= offsetX
+  if slopeZRatio > 0 and level % slopeZRatio == 0:
+    dz += offsetZ
+    widthZ -= offsetZ
   
   
 #eof while
