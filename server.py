e29b092cb66278b13095454e8382c7234db37967
minetonight <aleksandarnp@gmail.com>
2014-05-05 22:00:02 +0300

    public access to mython server

--- server.py
+++ server.py
@@ -9,14 +9,16 @@
 # 127.0.0.1 is a special address that means connect to the same machine I'm running this on
 # so this is useful when you are runnning the script and minecraft on the same machine
 address = "127.0.0.1"
+
 # If you are running the scripts on a machine other than the raspberry pi, then put the rasberry pi's
 # ip address here
-#address = "192.168.1.100"
+address = "192.168.1.21"
+address = "aleksandarnp.no-ip.biz"
 
 # If you are developing/testing against the RaspberyJuice Bukkit server, you may find that certain APIs are not
 # implemented like GetBlockWithData.
 # By setting the flag below to true or false, a script can use conditional logic on which features to use.
 # the default is set to True meaning the server is running Minecraft PI or on a Rasperbby PI.
 
-is_pi = True
-#is_pi = False
+#is_pi = True
+is_pi = False
