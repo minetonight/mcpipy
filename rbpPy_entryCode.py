ec4973b27cf0864f048b13719b799770bc9bc126
minetonight <aleksandarnp@gmail.com>
2014-07-17 13:04:34 +0300

    Using hash of the group name as an enrollment key.

--- rbpPy_entryCode.py
+++ rbpPy_entryCode.py
@@ -1,16 +1,8 @@
-# Get the ASCII number of a character
-secret = []
-for char in "www.bit.ly/rbp_mc_faq":
-  number = ord(char)
-  secret.append(number)
+import hashlib
 
-print secret
-#secret = [119, 119, 119, 46, 98, 105, 116, 46, 108, 121, 47, 114, 98, 112, 95, 109, 99, 95, 102, 97, 113]
+groupname = "140 City Py - int 00:00"
 
-# Get the character given by an ASCII number
-message = ""
-for i in secret:
-  char = chr(i) 
-  message += char
-
-print message
+# Assumes the default UTF-8
+hash_object = hashlib.md5(groupname.encode())
+print("The self-enrollment key for group %s is:" % (groupname))
+print(hash_object.hexdigest()[:8])
