3941f0954213298aaeac910f11229af94c38e0f8
Aleks <aleksandarnp@gmail.com>
2014-07-17 13:14:01 +0300

    comments: hashing works in both python 2 and 3

--- rbpPy_entryCode.py
+++ rbpPy_entryCode.py
@@ -1,8 +1,9 @@
+#this code works both with python 2 and python 3
 import hashlib
 
+#Change your group name here as given in the email.
 groupname = "140 City Py - int 00:00"
 
-# Assumes the default UTF-8
 hash_object = hashlib.md5(groupname.encode())
 print("The self-enrollment key for group %s is:" % (groupname))
 print(hash_object.hexdigest()[:8])
