3fe514284fd553c8f9784d7c0447f16ec0d15b07
minetonight <aleksandarnp@gmail.com>
2014-07-25 18:02:28 +0300

    added hitting and dice 6; added clear play area script

--- rbp_MC_NeSeSardiChoveche.py
+++ rbp_MC_NeSeSardiChoveche.py
@@ -6,7 +6,7 @@ import time #import time, so delays can be used
 import server # defines where we connect, check server.py
 import random
 
-daljina_pole = 50
+daljina_pole = 20
 broy_igrachi = input("Vavedi broya igrachi <7: ")
 pozicii = [0] * broy_igrachi
 
@@ -15,7 +15,6 @@ blokove_igrachi = [
   block.NETHER_BRICK_FENCE  ,
   block.COBBLESTONE_WALL,
   block.MOSSY_COBBLESTONE_WALL,
-  block.REDSTONE_TORCH_OFF,
   block.REDSTONE_TORCH_ON,
   block.TORCH,
   ]
@@ -24,21 +23,28 @@ tekusht_igrach = 0
 student_index = 0
 
 mc = minecraft.Minecraft.create(server.address)
-mc.postToChat("Hello МЦ Не се сърди Човече")
+mc.postToChat("МЦ Не се сърди Човече")
 
 #create field
+mc.setBlocks(student_index-1, 0, 0,  student_index+1, 2, daljina_pole, block.AIR)
 mc.setBlocks(student_index, 0, 0,  student_index, 0, daljina_pole, block.WOOL)
 time.sleep(3)
 
 while True:
     zar = random.randint(1, 6)
+    mc.postToChat("Играч %s хвърли зар %s" % (tekusht_igrach, zar))
     if pozicii[tekusht_igrach] + zar <= daljina_pole:
         nova_poziciya = pozicii[tekusht_igrach] + zar
+        for index_stari_pozicii in range(broy_igrachi):
+             if nova_poziciya == pozicii[index_stari_pozicii]:
+                 pozicii[index_stari_pozicii] = 0
+                 mc.postToChat("Играч %s удари играч %s" % (tekusht_igrach, index_stari_pozicii))
         pozicii[tekusht_igrach] = nova_poziciya
-        
-        sledvasht_igrach = tekusht_igrach + 1
-        if sledvasht_igrach == broy_igrachi:
-            sledvasht_igrach = 0
+    
+    
+    sledvasht_igrach = tekusht_igrach + 1
+    if sledvasht_igrach == broy_igrachi:
+        sledvasht_igrach = 0
            
     for tekushta_poziciya in range(1, daljina_pole + 1):
         block_za_izvejdane = block.AIR
@@ -47,12 +53,12 @@ while True:
                 block_za_izvejdane = blokove_igrachi[index_igrach]
         mc.setBlock(student_index, 1, tekushta_poziciya, block_za_izvejdane)
 
-    print(tekusht_igrach)
     if pozicii[tekusht_igrach] >= daljina_pole:
-        mc.postToChat("Specheli igrach: " + str(tekusht_igrach))
+        mc.postToChat("Спечели играч: " + str(tekusht_igrach))
         break
 
-    tekusht_igrach = sledvasht_igrach 
+    if zar != 6:
+      tekusht_igrach = sledvasht_igrach 
     time.sleep(2)
 
 
