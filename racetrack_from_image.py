6b78f2e022aca9dd83262f031097cb763d11510e
minetonight <aleksandarnp@gmail.com>
2015-09-21 11:22:39 +0300

    pseudo-code for minecraft python

--- racetrack_from_image.py
+++ racetrack_from_image.py
@@ -12,6 +12,8 @@ print height
 for x in range(width):
     for y in range(height):
         print(pixels[x, y],)
+        #if pixels[x, z][3] > 0:
+            #mc.setBlock(x, 80, z, Block.GRAVEL)
     print ("\n")
 
 # for x in 
